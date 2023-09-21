cd704b1cb701a99f6adfb569c4270328f0767eca
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-09-21 15:03:58 -0300

    Conectado o front-end ao back-end

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require("express")
 const app = express()
 const PORTA = 8080
+const http = require("http")
 const socketIO = require("socket.io")
 
-app.listen(PORTA,()=>{
+app.use(express.static("public"))
+
+const server = app.listen(PORTA,()=>{
     console.log(`O servidor está rodando na porta: ${PORTA}`)
+})
+
+const io = socketIO(server)
+
+io.on("connection",(socket)=>{
+    console.log("Nova conexão")
 })
